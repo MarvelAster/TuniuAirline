43671863caa347e5e860b9669337277dc3c278dc
Zhongda Gan <zhongdagan@oakland.edu>
2018-06-09 20:51:50 -0500

    day2 userProfile

--- Controller/FirebaseHandler.swift
+++ Controller/FirebaseHandler.swift
@@ -14,7 +14,18 @@ import FirebaseAuth
 import FirebaseStorage
 
 final class FirebaseHandler{
+    
+    var userRef : DatabaseReference!
+    var airportRef : DatabaseReference!
+    var userStorage: StorageReference!
+    
     static let sharedInstance = FirebaseHandler()
+    
+    func databaseInit() {
+        userRef = Database.database().reference().child("UserInfo")
+        airportRef = Database.database().reference().child("Airport")
+        userStorage = Storage.storage().reference()
+    }
     
     private init(){
         
@@ -56,5 +67,60 @@ final class FirebaseHandler{
         })
         let userImageDict = ["UserImage": "UserImage/\((Auth.auth().currentUser?.uid)!)"]
         userRef?.child("UserInfo").child((Auth.auth().currentUser?.uid)!).updateChildValues(userImageDict)
+    }
+    
+    func googleSignInUploadUserInfo(userId: String, name: String, email: String, userRef: DatabaseReference?) {
+        let dict = ["Email": email, "FullName": name, "UserId": userId, "UserImage": ""]
+        userRef?.child("UserInfo").child(userId).updateChildValues(dict)
+    }
+    
+    func checkIfCurrentUserExistWhenGoogleSignIn(userId : String, userRef: DatabaseReference? ,completion :@escaping (Bool) -> Void) {
+        userRef?.child(userId).observeSingleEvent(of: .value, with: {
+            (snapshot) in
+            var result = false
+            if (snapshot.value as? Dictionary<String, Any>) != nil {
+                result = true
+            }
+            completion(result)
+        })
+    }
+    
+    func downloadUserInfoInProfileViewController(completion:@escaping (String, String, UIImage)->Void) {
+        databaseInit()
+        userRef.child((Auth.auth().currentUser?.uid)!).observeSingleEvent(of:  .value) { (snapshot) in
+            guard let value = snapshot.value as? Dictionary<String, Any> else{
+                return
+            }
+            let name = value["FullName"] as! String
+            let email = value["Email"] as! String
+            let imagePath = value["UserImage"] as! String
+            self.userStorage.child(imagePath).getData(maxSize: 1024*1024*1024, completion: { (data, error) in
+                if error != nil{
+                    let image = UIImage(named: "userDefaultImage")
+                    completion(name, email, image!)
+                }else{
+                    let image = UIImage(data: data!)
+                    completion(name, email, image!)
+                }
+            })
+        }
+    }
+    
+    func uploadUserNameInProfileViewController(name: String) {
+        databaseInit()
+        let nameDict = ["FullName": name]
+        userRef.child((Auth.auth().currentUser?.uid)!).updateChildValues(nameDict)
+    }
+    
+    func uploadUserImageInProfileViewController(image: UIImage) {
+        databaseInit()
+        let data = UIImagePNGRepresentation(image)
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpg"
+        userStorage?.child("UserImage").child((Auth.auth().currentUser?.uid)!).putData(data!, metadata: metadata, completion: { (meta, error) in
+            if let err = error{
+                TWMessageBarManager().showMessage(withTitle: "Error", description: err.localizedDescription, type:  .error)
+            }
+        })
     }
 }
--- Controller/ProfileViewController.swift
+++ Controller/ProfileViewController.swift
@@ -8,8 +8,56 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var accountLabel: UILabel!
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        FirebaseHandler.sharedInstance.downloadUserInfoInProfileViewController{ (name, email, image) in
+            self.accountLabel.text = email
+            self.userImage.image = image
+            self.nameTextField.text = name
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        FirebaseHandler.sharedInstance.uploadUserNameInProfileViewController(name: textField.text!)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            userImage.image = pickedImage
+            FirebaseHandler.sharedInstance.uploadUserImageInProfileViewController(image: pickedImage)
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func selectImageBtnAction(_ sender: UIButton) {
+        imagePicker.allowsEditing = true
+        if imagePicker.sourceType ==  .camera{
+            imagePicker.sourceType =  .camera
+        }else{
+            imagePicker.sourceType =  .photoLibrary
+        }
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func resetPasswordBtnAction(_ sender: UIButton) {
+    }
+    
     @IBAction func btnClick(_ sender: Any) {
         let slidingViewController = self.slidingViewController()
         if slidingViewController?.currentTopViewPosition == .centered {
@@ -18,26 +66,6 @@ class ProfileViewController: UIViewController {
             slidingViewController?.resetTopView(animated: true)
         }
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
