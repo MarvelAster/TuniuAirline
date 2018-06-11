2f610dd7119a30ac1b11425d4d9d6f35e90af63e
Zhongda Gan <zhongdagan@oakland.edu>
2018-06-10 23:01:15 -0500

    bug fix

--- Controller/HomeViewController.swift
+++ Controller/HomeViewController.swift
@@ -8,8 +8,15 @@
 
 import UIKit
 import ECSlidingViewController
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController, UIScrollViewDelegate {
+    
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var image : [String] = ["coupon1", "coupon2", "coupon3", "coupon4"]
+    var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+    
     @IBAction func btnClick(_ sender: Any) {
         let slidingViewController = self.slidingViewController()
         if slidingViewController?.currentTopViewPosition == .centered {
@@ -20,14 +27,35 @@ class HomeViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        scrollViewSetup()
+        let singleTap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        singleTap.cancelsTouchesInView = false
+        singleTap.numberOfTapsRequired = 1
+        scrollView.addGestureRecognizer(singleTap)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let pageNumber = scrollView.contentOffset.x / scrollView.frame.size.width
+        pageControl.currentPage = Int(pageNumber)
     }
-
+    
+    func scrollViewSetup() {
+        pageControl.numberOfPages = image.count
+        for index in 0..<image.count{
+            frame.origin.x = scrollView.frame.size.width * CGFloat(index)
+            frame.size = scrollView.frame.size
+            
+            let imageView = UIImageView(frame: frame)
+            imageView.image = UIImage(named: image[index])
+            self.scrollView.addSubview(imageView)
+        }
+        scrollView.contentSize = CGSize(width: (scrollView.frame.size.width * CGFloat(image.count)), height: scrollView.frame.size.height)
+        scrollView.delegate = self
+    }
+    
+    @objc func handleTap(_ recognizer: UITapGestureRecognizer) {
+        print("11111")
+    }
 
 }
 
